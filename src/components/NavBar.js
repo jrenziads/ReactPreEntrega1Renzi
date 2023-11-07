913fa19cb31c34f7aa257a2578f3031b223e280d
jrenziads <128422655+jrenziads@users.noreply.github.com>
2023-11-07 20:48:25 -0300

    para entrega

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,34 @@
 import CartWidget from "./CartWidget";
 const NavBar = () => {
   return (
+    <div>
+    <nav className="navbar" role="navigation" aria-label="main navigation">
+    <div className="navbar-brand">
+      <a className="navbar-item" href="#">
+        <img src="ruta-del-logotipo.png" alt="Logotipo" width="112" height="28" />
+      </a>
+
+      <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+
+    <div id="navbarBasicExample" className="navbar-menu">
+      <div className="navbar-start">
+        <a className="navbar-item" href="#">
+          Inicio
+        </a>
+        <a className="navbar-item" href="#">
+          Servicios
+        </a>
+        <a className="navbar-item" href="#">
+          Productos
+        </a>
+      </div>
+    </div>
+  </nav>
     <div className="navbar-menu">
     <div className="navbar-start">
       <div className="navbar-item has-dropdown is-hoverable">
@@ -14,7 +42,10 @@ const NavBar = () => {
         </div>
       </div>
     </div>
+
     <CartWidget/>
+    
+  </div>
   </div>
   )
 };
